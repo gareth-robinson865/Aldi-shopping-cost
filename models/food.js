26ae51db6fa0eca8ba2fa78ffc0a5d9def50f784
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-07-07 21:03:42 +0100

    food model and schema created

---
+++ models/food.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const foodSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+const Food = mongoose.model('food', foodSchema);
+module.exports = Food;
