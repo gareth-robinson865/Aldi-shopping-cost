9e426f7b0df9c7493d0a074d79f349b108110335
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-07-07 21:19:30 +0100

    basic route created for about page

--- server.js
+++ server.js
@@ -36,3 +36,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.render('index', { title: 'home'})
 })
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'about'})
+})
