784451b67ce366afc12af97f8fca414471385d8e
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-07-12 10:56:01 +0100

    hidden but now error with connection

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const mongoose = require('mongoose');
 const app = express();
 
 //connect to mongoDB
-const dbURI = 'mongodb+srv://new-user:Test1234@nodestuff.ykrvs.mongodb.net/FoodStuff?retryWrites=true&w=majority';
+const dbURI = process.env.dbURI
+    .next();
 mongoose.connect(dbURI)
     .then((result) => app.listen('3000'))//only listen for requests once connection to the db has been made
     .catch((err) => console.log(err));
