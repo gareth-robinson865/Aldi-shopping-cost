7d22cbadce780989155cba436a53308d93024f84
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-08-09 18:37:16 +0100

    moved the index function from the main server file to the controller file

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,5 +1,11 @@
 const Food = require('../models/food');
 
 const food_index = (req, res) => {
-    
+    Food.find().sort({ aisle: -1 })
+        .then((result) => {
+            res.render('index', { title: 'home', foods: result })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
