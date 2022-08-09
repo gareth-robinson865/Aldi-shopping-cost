920993c86799a29c0fec8d483923107fc8ab8c6b
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-08-09 19:32:09 +0100

    food routes file created to handle the routes

---
+++ routes/foodRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const foodController = require('../controllers/foodController');
+
+const router = express.Router();
+
+//!Food routes
+router.get('/', foodController.food_index);
+
+router.post('/', foodController.food_create_post);
+
+router.get('/:id', foodController.food_details);
+
+router.delete('/:id', foodController.food_delete);
+
+module.exports = router;
