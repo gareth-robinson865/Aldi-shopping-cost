b7d83b2e95f04495808f997bbe52032fce9c9400
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-08-14 11:51:04 +0100

    route path changed in the routes file to reflect the new change. tested and found to be working

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -8,7 +8,7 @@ router.get('/', foodController.food_index);
 
 router.post('/', foodController.food_create_post);
 
-router.get('/details/:id', foodController.food_details);
+router.get('/:id', foodController.food_details);
 
 router.delete('/:id', foodController.food_delete);
 
