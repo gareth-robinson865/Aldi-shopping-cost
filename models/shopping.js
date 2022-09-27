c0990451f26d3a9925382f932aab797af542de52
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-09-27 16:17:43 +0100

    schema created

--- models/shopping.js
+++ models/shopping.js
@@ -2,5 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const shoppingSchema = new Schema({
-    
-})
+    name: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    aisle: {
+        type: Number
+    }
+}, { timestamps: true });
+
