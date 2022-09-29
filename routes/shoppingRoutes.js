bf8b78bdef4b2bb258a0138aca625b291b85053b
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-09-29 16:38:34 +0100

    post statement created and exported

--- routes/shoppingRoutes.js
+++ routes/shoppingRoutes.js
@@ -1,3 +1,8 @@
 const express = require('exporess');
 const shoppingController = require('../controllers/shoppingController');
 
+const shoppingRouter = express.Router();
+
+router.post('/', shoppingController.shopping_create_post);
+
+module.exports = shoppingRouter;
