10a5178e4980c1bbc85087014a259c26310285fa
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-09-29 17:15:43 +0100

    remedy attempt by creating a function for the post request

--- routes/shoppingRoutes.js
+++ routes/shoppingRoutes.js
@@ -3,6 +3,8 @@ const shoppingController = require('../controllers/shoppingController');
 
 const shoppingRouter = express.Router();
 
-shoppingRouter.post('/', shoppingController.shopping_create_post);
+shoppingRouter.post('/', function(req, res){
+    shoppingController.shopping_create_post
+});
 
 module.exports = shoppingRouter;
