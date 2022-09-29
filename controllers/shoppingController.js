409c871d63ed23e8e69dc3af71c3f33ef8781398
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-09-29 16:17:24 +0100

    create post code added

--- controllers/shoppingController.js
+++ controllers/shoppingController.js
@@ -1,5 +1,11 @@
 const Shopping = require('../models/shopping');
 
 const shopping_create_post = (req, res) => {
-    
+    console.log(req.body)
+    const item = new Shopping(req.body)
+    item.save()
+        .then((result)) 
+        .catch((err) => {
+            console.log(err)
+        })
 }
