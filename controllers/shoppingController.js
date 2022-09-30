3cf3ba8850dc83d1c607eb9ac7cb37f6d8f18863
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-09-30 20:44:57 +0100

    test console log statement created

--- controllers/shoppingController.js
+++ controllers/shoppingController.js
@@ -2,6 +2,7 @@ const Shopping = require('../models/shopping');
 
 const shopping_create_post = (req, res) => {
     console.log(req.body)
+    console.log('creating something');
     const item = new Shopping(req.body)
     item.save()
         .then((result)) 
