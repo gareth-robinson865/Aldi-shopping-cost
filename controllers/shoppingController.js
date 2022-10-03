05be0875af4360a5cc01f0afc3e0d0179700407a
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-10-03 13:41:19 +0100

    create post current content commented out for testing

--- controllers/shoppingController.js
+++ controllers/shoppingController.js
@@ -1,14 +1,17 @@
 const Shopping = require('../models/shopping');
 
 const shopping_create_post = (req, res) => {
-    console.log(req.body)
-    console.log('creating something');
-    const item = new Shopping(req.body)
-    item.save()
-        .then((result)) 
-        .catch((err) => {
-            console.log(err)
-        })
+    
+    
+    
+    //console.log(req.body)
+    //console.log('creating something');
+    //const item = new Shopping(req.body)
+    //item.save()
+    //    .then((result)) 
+    //    .catch((err) => {
+    //        console.log(err)
+    //    })
 }
 
 module.exports = shopping_create_post;
