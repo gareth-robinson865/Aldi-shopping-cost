1ca31e4ba6ac5f2fde344a55bef8abe90aab56c1
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-10-12 09:03:23 +0100

    comment added to show a to do when get home

--- public/javascript/foodToList.js
+++ public/javascript/foodToList.js
@@ -3,4 +3,6 @@ const Food = require('../../models/food');
 const searchFood = () => {
     const searchName = document.getElementsByClassName('searchButton').value
     console.log(searchName);
+    //!TODO need to check the searchname value is being taken
+    
 }
