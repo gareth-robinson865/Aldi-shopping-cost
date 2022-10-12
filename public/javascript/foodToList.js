aef55e4e3168d5fa5bf7dbab675183a353e52b20
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-10-12 08:52:07 +0100

    code added to get the value that has been entered into the textbox

--- public/javascript/foodToList.js
+++ public/javascript/foodToList.js
@@ -1,5 +1,6 @@
 const Food = require('../../models/food');
 
 const searchFood = () => {
-    
+    const searchName = document.getElementsByClassName('searchButton').value
+    console.log(searchName);
 }
