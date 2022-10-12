56cfe0c0679d4ca16c779db536fe0fecc22bf939
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-10-12 09:05:18 +0100

    find one search added but needs to be tested

--- public/javascript/foodToList.js
+++ public/javascript/foodToList.js
@@ -4,5 +4,5 @@ const searchFood = () => {
     const searchName = document.getElementsByClassName('searchButton').value
     console.log(searchName);
     //!TODO need to check the searchname value is being taken
-    
+    Food.findOne(searchFood)
 }
