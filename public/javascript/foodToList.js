332c07e3ad16d78ddc6479a84b9230bd56c3ed46
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-10-12 09:22:24 +0100

    li items created in the function

--- public/javascript/foodToList.js
+++ public/javascript/foodToList.js
@@ -1,9 +1,13 @@
 const Food = require('../../models/food');
 
 const searchFood = () => {
+    //getting the value from the search item
     const searchName = document.getElementsByClassName('searchButton').value
     console.log(searchName);
     //!TODO need to check the searchname value is being taken
+    const name = document.createElement('li')
+    const price = document.createElement('li')
+    const aisle = document.createElement('li')
     Food.findOne(searchFood)
     //!TODO need to check that the search is coming through
 }
